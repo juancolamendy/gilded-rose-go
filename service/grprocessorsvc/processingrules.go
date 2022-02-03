8730a44428469e9e6f34e063401897c839b073ce
Juan Carlos Olamendy <juan.olamendy@bluvision.com>
2022-02-03 10:11:14 -0500

    implement rules

--- service/grprocessorsvc/processingrules.go
+++ service/grprocessorsvc/processingrules.go
@@ -17,9 +17,9 @@ type ruleFn func(item *Item)
 
 var (
 	rules = map[string]ruleFn {
-		AGED_BRIE: defaultRule,
-		LEGENDARY: defaultRule,
-		BACKSTAGE: defaultRule,
+		AGED_BRIE: defaultAgedBrie,
+		LEGENDARY: defaultLegendary,
+		BACKSTAGE: defaultBackstage,
 	}
 )
 
@@ -42,5 +42,56 @@ func getProcessingRule(item *Item) ruleFn {
 }
 
 func defaultRule(item *Item) {
-	log.Printf("--- Applying default rule to item: %s", item.String())
+	log.Printf("--- Applying 'default' rule to item: %s", item.String())	
+	// default adjustment
+	adjustment := -1
+	if item.days <= 0 {
+		adjustment = -2
+	}
+	// apply rules
+	// adjust day
+	item.days = item.days - 1
+	// adjust quality
+	item.quality = adjust(item.quality, adjustment, 0, 50)
+}
+
+func defaultAgedBrie(item *Item) {
+	log.Printf("--- Applying 'aged brie' rule to item: %s", item.String())	
+	// default adjustment
+	adjustment := 1
+	if item.days <= 0 {
+		adjustment = 2
+	}
+	// apply rules
+	// adjust day
+	item.days = item.days - 1
+	// adjust quality
+	item.quality = adjust(item.quality, adjustment, 0, 50)
+}
+
+func defaultLegendary(item *Item) {
+	log.Printf("--- Applying 'legendary' rule to item: %s", item.String())
+	// noop
+}
+
+func defaultBackstage(item *Item) {
+	log.Printf("--- Applying 'backstage' rule to item: %s", item.String())
+	// apply rules
+	// default adjustment
+	adjustment := 0
+	if item.days > 10 {
+		adjustment = 1
+	} else if item.days > 5 {
+		adjustment = 2
+	} else if item.days > 0 {
+		adjustment = 3
+	}
+	// adjust day
+	item.days = item.days - 1
+	// adjust quality
+	if adjustment == 0 {
+		item.quality = 0
+		return
+	}	
+	item.quality = adjust(item.quality, adjustment, 0, 50)
 }
