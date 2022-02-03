695807ab30041de97288712d8684b85ee795e910
Juan Carlos Olamendy <juan.olamendy@bluvision.com>
2022-02-03 06:47:02 -0500

    test/initial struct

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 run:
+	@echo "Running the application"
 	go run cmd/main.go
+
+test:
+	@echo "Testing the application"
+	go test -v ./...
--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,11 @@ package main
 
 import(
 	"fmt"
+
+	"github.com/juancolamendy/gilded-rose-go/service/grprocessorsvc"
 )
 
 func main(){
+	grprocessorsvc.Process()
 	fmt.Println("Hello world")
 }
