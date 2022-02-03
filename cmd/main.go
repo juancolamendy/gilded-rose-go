cabc5b9e76407004a4d99fdf88acb3b59db2a0ce
Juan Carlos Olamendy <juan.olamendy@bluvision.com>
2022-02-03 12:25:28 -0500

    add input

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,31 @@ import(
 	"github.com/juancolamendy/gilded-rose-go/service/grprocessorsvc"
 )
 
+var things = []struct {
+	name    string
+	sellIn  int
+	quality int
+}{
+	{"+5 Dexterity Vest", 10, 20},
+	{"Aged Brie", 2, 0},
+	{"Elixir of the Mongoose", 5, 7},
+	{"Sulfuras, Hand of Ragnaros", 0, 80},
+	{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+	{"Conjured Mana Cake", 3, 6},
+}
+
 func main(){
-	item := grprocessorsvc.New("normal", 5, 10)
-	grprocessorsvc.Process(item)
-	fmt.Printf("Result item: %s\n", item.String())
+	var items []*grprocessorsvc.Item
+
+	for _, thing := range things {
+		item := grprocessorsvc.New(thing.name, thing.sellIn, thing.quality)
+		items = append(items, item)
+		fmt.Println(item)
+	}
+
+	grprocessorsvc.Process(items...)
+
+	for _, item := range items {
+		fmt.Println(item)
+	}
 }
